17f536271de0b937af6975855c7fc916486e3214
Debarshi Acharya <technical.learning23@gmail.com>
2025-05-19 05:09:25 +0000

    correlation reference changes

--- models/weather_bike_correlation.sql
+++ models/weather_bike_correlation.sql
@@ -1,6 +1,8 @@
 WITH CTE AS (
-    select *
-    from {{ ref('trip_fact') }}
+    select t.*
+    from {{ ref('trip_fact') }} t
+    left join {{ ref('daily_weather') }} w
+    on t.trip_date = w.daily_weather
     limit 10
 )
 
